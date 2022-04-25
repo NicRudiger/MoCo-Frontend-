16d53871e455340be36d17b69efd3bb85318112e
Emmet Ryan <emmetryan7@gmail.com>
2022-04-25 18:07:08 -0400

    Created basic server code with working connection to index

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const http = require('http')
+const fs = require('fs')
 const app = express()
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-  });
-  
-  app.listen(port, () => {
-    console.log("server started on port 3000");
-  });
+http.createServer(function(req, res){
+  res.writeHead(200, {'Content-Type': "text/html"})
+  const file = fs.createReadStream('index.html')
+  file.pipe(res)
+}).listen(port);
+
+console.log("server started on port 3000");
