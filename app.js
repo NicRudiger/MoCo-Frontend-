17c10ef1570b4cb6df493656be3a2bb761bf0bdb
Emmet Ryan <emmetryan7@gmail.com>
2022-04-25 17:14:21 -0400

    Moved some files around and created working server

--- app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => res.send("Welcome to setting up Node.js project tutorial!'))
-
-app.listen(port, () => console.log(`Application listening on port ${port}!`))
