b95c096afbc18049d20e3b853675abddbe0931c8
Emmet Ryan <emmetryan7@gmail.com>
2022-04-27 08:28:13 -0400

    Completed preliminary server work

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express')
-const http = require('http')
-const fs = require('fs')
-const app = express()
+const reload = require('livereload')
+const dotenv = require('dotenv')
+const path = require('path')
 
-const port = process.env.PORT || 3000;
+dotenv.config();
+const dirname = path.resolve();
 
-http.createServer(function(req, res){
-  res.writeHead(200, {'Content-Type': "text/html"})
-  const file = fs.createReadStream('index.html')
-  file.pipe(res)
-}).listen(port);
+const app = express();
+const PORT = process.env.PORT || 3000;
+const staticFolder = 'client';
+let liveReloadServer;
 
-console.log("server started on port 3000");
+if (process.env.CONTEXT === 'development') {
+  liveReloadServer = reload.createServer();
+  liveReloadServer.watch(path.join(dirname, staticFolder));
+}
+
+app.use(express.static(staticFolder));
+
+async function bootServer() {
+  try {
+    app.listen(PORT, async () => {
+      console.log(`Listening on: http//localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+bootServer();
