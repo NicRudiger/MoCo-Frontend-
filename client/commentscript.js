198cf90a328b9fb1991b0707340f5e743e67f894
Emmet Ryan <emmetryan7@gmail.com>
2022-05-12 22:33:22 -0400

    Finished work on comments

--- client/commentscript.js
+++ client/commentscript.js
@@ -8,9 +8,16 @@ document.getElementById('submit').addEventListener('click', (event) => {
   async function handleButtonSubmit() {
     const user_pass = prompt("Enter your password to add a comment");
     if (user_pass == password) {
+        const table_select = document.querySelector('input[name="table"]:checked').value;
         const rec_id = document.getElementById('rec_id').value;
         const comment = document.getElementById('comment').value;
-        const rec_comment = await fetch(`/api/tfrecs/${rec_id}/${comment}`)
+        if (table_select == "RPS") {
+            const rec_comment = await fetch(`/api/tfrecs/${rec_id}/${comment}`)   
+        } else if (table_select == "MPAA") {
+            const mpaa_comment = await fetch(`/api/mpaa/${rec_id}/${comment}`)
+        } else if (table_select == "Audit"){
+            const audit_comment = await fetch(`/api/audit/${rec_id}/${comment}`)
+        }
         alert("Comment successfully added")
     } else {
         alert("Incorrect password. Please try again");
--- server/apiRoutes.js
+++ server/apiRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import Pool from 'pg-pool';
 const router = express.Router();
 
-const connectStr = process.env.DATABASE_URL
+const connectStr = `postgres://caeltaetxhemre:fce0691b7d6c3ac33a16fd72ec7a4009ad74b3410385a6b75c488dc3ce39c157@ec2-34-194-158-176.compute-1.amazonaws.com:5432/de09islg90c56t`
 const pool = new Pool({
     connectionString: connectStr,
     ssl: {
@@ -15,38 +15,54 @@ router.get('/', (req, res) => {
   });
 
 router.get('/tfrecs', async (req, res) => {
-    const tf_recs = await pool.query('SELECT * FROM tf_recs')
+    const tf_recs = await pool.query('SELECT * FROM tf_recs ORDER BY action_id ASC')
     res.send(tf_recs.rows)
   });
 
 router.get('/mpaa', async (req, res) => {
-  const mpaa = await pool.query('SELECT * FROM mpaa')
+  const mpaa = await pool.query('SELECT * FROM mpaa ORDER BY mpaa_id ASC')
   res.send(mpaa.rows)
 });
 
 router.get('/audit', async (req, res) => {
-  const audit = await pool.query('SELECT * FROM audit')
+  const audit = await pool.query('SELECT * FROM audit ORDER BY audit_id ASC')
   res.send(audit.rows)
 });
 
 router.get('/tfrecs/:rec_id/:comment', async (req, res) => {
-  const updateRecs = async (comment, rec_id) => {
-    const query = `UPDATE "tf_recs" 
-                   SET "ssjc_comments" = $1
-                   WHERE "action" = $2`;
-    try {
-        await pool.query(query, [comment, rec_id]);
-        return true;
-    } catch (error) {
-        console.log(error);
-        return false;
-    }
+  const text = 'UPDATE tf_recs SET ssjc_comments = $1 WHERE action_id = $2 RETURNING *'
+  const values = [req.params.comment, req.params.rec_id]
+  try {
+    const update = await pool.query(text, values)
+    console.log(update.rows[0])
+    res.send(update.rows[0])
+  } catch (err) {
+    console.log(err.stack)
   }
-  updateRecs(req.params.comment, req.params.rec_id).then(result => {  // userName, userRole, userId
-    if (result) {
-        console.log(result);
-    }
 });
+
+router.get('/mpaa/:rec_id/:comment', async (req, res) => {
+  const text = 'UPDATE mpaa SET ssjc_comments = $1 WHERE mpaa_id = $2 RETURNING *'
+  const values = [req.params.comment, req.params.rec_id]
+  try {
+    const update = await pool.query(text, values)
+    console.log(update.rows[0])
+    res.send(update.rows[0])
+  } catch (err) {
+    console.log(err.stack)
+  }
+});
+
+router.get('/audit/:rec_id/:comment', async (req, res) => {
+  const text = 'UPDATE audit SET ssjc_comments = $1 WHERE audit_id = $2 RETURNING *'
+  const values = [req.params.comment, req.params.rec_id]
+  try {
+    const update = await pool.query(text, values)
+    console.log(update.rows[0])
+    res.send(update.rows[0])
+  } catch (err) {
+    console.log(err.stack)
+  }
 });
 
 export default router;
