dc82b83e5968b3b061432a9eb31fe0e198692e7b
jscoll <98493265+jscoll@users.noreply.github.com>
2022-11-13 21:53:56 -0500

    Improve variable name readability

--- database/init.js
+++ database/init.js
@@ -7,20 +7,15 @@ import models from '../models/index.js';
 const env = process.env.ENVIRONMENT || 'development';
 const opts = config[env];
 
-const sequelize = new Sequelize(
-  opts.database,
-  opts.username,
-  opts.password,
-  opts
-);
+const seq = new Sequelize(opts.database, opts.username, opts.password, opts);
 
 // Construction dictionary of imported models
 const db = Object.keys(models).reduce((collection, name) => {
-  collection[name] = models[name](sequelize, Sequelize.DataTypes);
+  collection[name] = models[name](seq, Sequelize.DataTypes);
   return collection;
 }, {});
 
 db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+db.sequelize = seq;
 
 export default db;
