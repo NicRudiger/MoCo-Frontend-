3985953bfb550132bb2ae0ef6427d5e5185ba3eb
jscoll <98493265+jscoll@users.noreply.github.com>
2022-11-13 23:15:11 -0500

    Add basic comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,10 +4,12 @@ import db from '../database/init.js';
 
 const router = express.Router();
 
+// Respond with a simple hello world!
 router.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+// Fetch all rows from 'mcpd_audit' table
 router.get('/mcpd_audit', async (req, res) => {
   try {
     const data = await db.mcpd_audit.findAll();
