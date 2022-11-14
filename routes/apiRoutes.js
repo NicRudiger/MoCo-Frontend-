ff7485be57849959ffc2e49de0c01a641be912ce
jscoll <98493265+jscoll@users.noreply.github.com>
2022-11-13 21:13:07 -0500

    Add basic mcpd_audit API endpoint

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,4 +8,14 @@ router.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+router.get('/mcpd_audit', async (req, res) => {
+  try {
+    const data = await db.mcpd_audit.findAll();
+    res.json(data)
+  } catch (e) {
+    console.error(e);
+    res.send('Error in GET /mcpd_audit')
+  }
+});
+
 export default router;
